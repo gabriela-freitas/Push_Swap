6a839b056204a290fa02c27dd176a3df116bad30
Gabriela Freitas de albuquerque <gafreita@c1r14s6.42lisboa.com>
2022-05-17 21:05:37 +0100

    new ideas to big sort - divide to conquer

--- new_big_sort.c
+++ new_big_sort.c
@@ -6,7 +6,7 @@
 /*   By: gafreita <gafreita@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 16:23:03 by gafreita          #+#    #+#             */
-/*   Updated: 2022/05/17 19:37:45 by gafreita         ###   ########.fr       */
+/*   Updated: 2022/05/17 21:03:10 by gafreita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,43 @@
 //and sorting stack B everytime I put something there
 
 static void	sort_b(t_info *stacks);
+static void	pass_five_to_b(t_info *stacks);
 
 void	new_big_sort(t_info *stacks)
 {
+	static int	begin = 0;
+	static int	end = 5;
+
 	if (stacks->size_a == stacks->all && check_last_move(stacks))
 		return ;
-	else
+	if (stacks->size_a > stacks->all / 2)
 	{
-		pass_to_b(stacks);
-		sort_b(stacks);
+		pass_five_to_b(stacks); //pass x numbers to b, test with different sizes of stacks
+		sort_small_b(stacks);
+		//update the sort small to work with anykind of stacks, searching for the bigger numbers, going back to a and then sorting 3 numbers.
 	}
-	new_big_sort(stacks);
+	if (end + 5 == stacks->all)
+		end = stacks->all;
+	else
+		end += 5;
+	new_big_sort(stacks, begin + 5, end);
 }
 
-static void	sort_b(t_info *stacks)
+static void	pass_five_to_b(t_info *stacks, int begin, int end)
 {
+	static int	check = begin;
 
+	if (check == end)
+		return ;
+	if (stacks->head_a->index < end || stacks->head_a->index >= begin)
+		check += pb(stacks) / 3;
+	else if (stacks->head_a->next->index < end
+		|| stacks->head_a->next->index >= begin)
+		sa(stacks);
+	else if (t_stack_last(stacks->head_a)->index < end
+		|| t_stack_last(stacks->head_a)->index >= begin)
+		rra(stacks);
+	else
+		ra(stacks);
+	pass_to_b(stacks, begin, end);
 }
